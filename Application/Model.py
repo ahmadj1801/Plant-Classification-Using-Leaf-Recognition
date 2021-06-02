2741c2ec765c3530266652f484654a61bfce2d89
Ahmad Jawaad Shah <ahmadj1801@gmail.com>
2021-06-03 00:55:04 +0200

    Pre processing

--- Application/Model.py
+++ Application/Model.py
@@ -20,6 +20,12 @@ def read_data_file():
     return dataset
 
 
+def graph_image(img):
+    pt.subplot(1, 2, 1)
+    pt.imshow(img)
+    pt.show()
+
+
 def graph_colour(c):
     pt.subplot(1, 2, 1)
     pt.imshow(rgb_to_hsv(c))
@@ -42,13 +48,9 @@ def graph_hsv(img):
 
 
 def image_pre_processing(df: pd.DataFrame):
-    # local variables for feeding pre processing
-    # original, grey, equalization, median, threshold = ''
-    global display
-    o1, o2, o3 = '', '', ''
     # Display the process of a random image in the data set
     seed(0)
-    selected = 775 # randint(0, len(df))
+    selected = 384  # randint(0, len(df))
     print('Selected Image Number = ', selected)
     # preprocess the images
     c = 0
@@ -56,48 +58,39 @@ def image_pre_processing(df: pd.DataFrame):
         print('Pre processing ', c, ' ', row['species'])
         # Original image
         original = cv2.imread(row['image_path'])
+        # Resize the image
         original = cv2.resize(original, (400, 400), interpolation=cv2.INTER_AREA)
-        # original = cv2.cvtColor(original, cv2.COLOR_GRAY2RGB)
+        # Convert to RGB
         original = cv2.cvtColor(original, cv2.COLOR_BGR2RGB)
         # Change the colour space
         hue = cv2.cvtColor(original, cv2.COLOR_RGB2HSV_FULL)
+        # Set upper and lower bounds for segmentation
         lg = (10, 50, 10)
         dg = (128, 255, 128)
-        light_square = np.full((10, 10, 3), lg, dtype=np.uint8)/255.0
-        dark_square = np.full((10, 10, 3), dg, dtype=np.uint8) / 255.0
-        # Convert to gray scale
-        grey = cv2.cvtColor(original, cv2.COLOR_RGB2HSV_FULL)
-        # Median Filtering
-        # median = cv2.medianBlur(grey, 5)  # cv2.GaussianBlur(threshold, (5,5), 0) #cv2.medianBlur(grey, 5)
-        # Threshold
-        # T, threshold = cv2.threshold(grey, 100, 255, type=cv2.THRESH_BINARY_INV)# cv2.threshold(median, 100, 255, type=cv2.THRESH_BINARY_INV)
-        # Resize
-        # cv2.resize(median, (400, 400), interpolation=cv2.INTER_AREA)
+        # Threshold segmentation
         mask = cv2.inRange(hue, lg, dg)
+        # Filtering noise
+        filtering = cv2.medianBlur(mask, 5)
+        # Display a random image
         if selected == c:
-            '''cv2.imshow('HUE STUFF', mask)
-            graph_colour(light_square)
-            graph_colour(dark_square)
-            grey = cv2.cvtColor(grey, cv2.COLOR_GRAY2BGR)
-            threshold = cv2.cvtColor(threshold, cv2.COLOR_GRAY2BGR)
-            median = cv2.cvtColor(median, cv2.COLOR_GRAY2BGR)
-            row1 = np.hstack((original, grey))
-            row2 = np.hstack((threshold, median))
-            display = np.vstack((row1, row2))'''
+            # Horizontal display
+            row = np.hstack((mask, filtering))
+            # Display RGB image
+            graph_image(original)
+            # Graph of HSV space
             graph_hsv(hue)
+            # Graph of threshold colours
+            light_square = np.full((10, 10, 3), lg, dtype=np.uint8) / 255.0
+            dark_square = np.full((10, 10, 3), dg, dtype=np.uint8) / 255.0
             graph_colour(light_square)
             graph_colour(dark_square)
-            cv2.imshow('Original Image', original)
-            cv2.imshow('HSV', hue)
-            pt.subplot(1,2,1)
-            pt.imshow(hue)
-            pt.show()
-            cv2.imshow('EDIT', mask)
-            '''cv2.imshow('Grey Image', grey)
-            cv2.imshow('Smoothed Image', median)
-            cv2.imshow('Threshold Image', threshold)'''
+            # Display HSV Image
+            graph_image(hue)
+            # Display
+            cv2.imshow('Pre Processing', row)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
+        # Counter
         c = c + 1
 
 
